94ce8308f0d2c09893d1032bdf7eae1e87433c49
Simen Haugerud Granlund <simen@hgranlund.com>
2017-09-02 20:26:56 +0200

    tester azure deploy

--- app.py
+++ app.py
@@ -45,5 +45,19 @@ class Endgame(Resource):
         return DAO.get(args.fen)
 
 
+
+def wsgi_app(environ, start_response):
+    status = '200 OK'
+    response_headers = [('Content-type', 'text/plain')]
+    start_response(status, response_headers)
+    response_body = 'Hello World'
+    yield response_body.encode()
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
