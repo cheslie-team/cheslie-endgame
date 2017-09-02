cc28d6f01f217037bd1488b00f2f2ee387824d4f
Simen Haugerud Granlund <simen@hgranlund.com>
2017-09-02 15:39:28 +0200

    Added the api with swagger

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cheslie-endgame
 A Syzygy endgame tablebase API
+
+
+## Setup development
+
+### Setup virtual environment
+
+        sudo apt-get install python-virtualenv
+        virtualenv cheslie-env
+        source cheslie-env/bin/activate
+        pip install -r requirements.txt
+
--- app.py
+++ app.py
@@ -1,12 +1,59 @@
-def wsgi_app(environ, start_response):
-    status = '200 OK'
-    response_headers = [('Content-type', 'text/plain')]
-    start_response(status, response_headers)
-    response_body = 'Hello World'
-    yield response_body.encode()
+from flask import Flask
+from flask_restplus import Api, Resource, fields
+
+app = Flask(__name__)
+api = Api(app, version='1.0', title='Cheslie endgame API',
+          description='Probe endgame databases',
+          )
+
+ns = api.namespace('v1', description='Cheslie endgame api')
+
+endgameModel = api.model('EndgameDB', {
+    'id': fields.String(readOnly=True, description='The endgame database unique identiier'),
+    'name': fields.String(required=True, description='Name of the database'),
+    'description': fields.String(required=True, description='Description')
+})
+
+
+class EndgameDAO(object):
+    def __init__(self):
+        self.endgames = []
+
+    def get(self, id):
+        for endgame in self.endgames:
+            if endgame['id'] == id:
+                return endgame
+        api.abort(404, "Endgame {} doesn't exist".format(id))
+
+    def create(self, data):
+        endgame = data
+        self.endgames.append(endgame)
+        return endgame
+
+
+DAO = EndgameDAO()
+DAO.create({'id': 'syzygy',
+            'name': 'Syzygy endgame database',
+            'description': 'Syzygy tablebases provide WDL and DTZ information for all endgame positions with up to 6 pieces, allowing you to make progress, keeping a win in hand, winning all won positions, bringing all drawn positions over the fifty-move line.'})
+
+
+@ns.route('/')
+class EndgameList(Resource):
+    @ns.doc('list_endgames')
+    @ns.marshal_list_with(endgameModel)
+    def get(self):
+        return DAO.endgames
+
+
+@ns.route('/<string:id>')
+@ns.response(404, 'Endgame database not found')
+@ns.param('id', 'The endgame database identifier')
+class Endgame(Resource):
+    @ns.doc('get_probe_results')
+    @ns.marshal_with(endgameModel)
+    def get(self, id):
+        '''Fetch a given resource'''
+        return DAO.get(id)
 
 if __name__ == '__main__':
-    from wsgiref.simple_server import make_server
-
-    httpd = make_server('localhost', 5555, wsgi_app)
-    httpd.serve_forever()
+    app.run(debug=True)
